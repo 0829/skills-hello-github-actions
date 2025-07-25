48dfc949d074bf66d18e8ceb33f2b0b560164058
surf <surf@mail.com>
2025-07-25 19:58:42 +0900

    add: vitest

---
+++ test/index.test.js
@@ -0,0 +1,12 @@
+import { test, expect } from 'vitest';
+import { add, multiply } from '../index.js';
+
+test('add function', () => {
+  expect(add(2, 3)).toBe(5);
+  expect(add(-1, 1)).toBe(0);
+});
+
+test('multiply function', () => {
+  expect(multiply(2, 3)).toBe(6);
+  expect(multiply(0, 5)).toBe(0);
+});
