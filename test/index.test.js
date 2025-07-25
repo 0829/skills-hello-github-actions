268e12c7b94b3cdb6906c4d9285b72f65e7e7ad8
surf <surf@mail.com>
2025-07-25 20:17:42 +0900

    wip: fail

--- test/index.test.js
+++ test/index.test.js
@@ -4,6 +4,7 @@ import { add, multiply } from '../index.js';
 test('add function', () => {
   expect(add(2, 3)).toBe(5);
   expect(add(-1, 1)).toBe(0);
+  expect(add(0, 10)).toBe(0);
 });
 
 test('multiply function', () => {
